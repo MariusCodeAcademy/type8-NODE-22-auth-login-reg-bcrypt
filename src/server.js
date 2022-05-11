4c44216fe81dc4bfc717d88b0e29237490012fb0
Marius <marius@example.com>
2022-05-11 09:31:25 +0300

    showBody middleware

--- src/server.js
+++ src/server.js
@@ -15,6 +15,16 @@ const users = [
 // MiddleWare
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(showBody);
+
+// first middlewere helper
+function showBody(req, res, next) {
+  // console.log(req.method);
+  if (req.method === 'POST') {
+    console.log('request body ===', req.body);
+  }
+  next();
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -33,6 +43,19 @@ app.post('/register', (req, res) => {
   console.log('users ===', users);
 });
 
+// POST /login - tuscias routas grazina 'bandom prisilogint'
+app.post('/login', (req, res) => {
+  res.json('bandom prisilogint');
+});
+
 // GET /users - grazina visus vartojus json formatu
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+// 404 turetu grazinti json objekta
+app.all('*', (req, res) => {
+  res.status(404).json({ error: 'Page not found' });
+});
 
 app.listen(PORT, () => console.log('server online on port', PORT));
