031020850a871a1f64a986fd388e731c20ead41b
Marius <marius@example.com>
2022-05-11 08:32:19 +0300

    server up and running.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-// server
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = 3000;
+
+// MiddleWare
+app.use(morgan('dev'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => console.log('server online on port', PORT));
