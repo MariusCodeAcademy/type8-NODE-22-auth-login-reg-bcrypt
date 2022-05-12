7409ec9d693be25b1e3eed6e6cda51fc81fa6ab8
Marius <marius@example.com>
2022-05-12 09:27:32 +0300

    register start fe

---
+++ public/register.js
@@ -0,0 +1,6 @@
+console.log('register here');
+
+// 1. paimti formos duomenis
+// 2. palytingi ar sutampa slaptazodziai
+// 3. jei sutampa siusti i registracijos endpointa
+// 4. pranesti apie klaida jei tokia ivyko
