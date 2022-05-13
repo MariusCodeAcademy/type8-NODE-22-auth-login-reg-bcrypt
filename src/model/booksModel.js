fc5283d7268ef2eecd7aebb072f53c4722d09063
Marius <marius@example.com>
2022-05-13 10:47:46 +0300

    authorBookCount

--- src/model/booksModel.js
+++ src/model/booksModel.js
@@ -15,8 +15,31 @@ async function getArrayFromDb(sql) {
   }
 }
 
+async function executeDb(sql, dataToDBArr) {
+  let conn;
+  try {
+    conn = await mysql.createConnection(dbConfig);
+    const [result] = await conn.execute(sql, dataToDBArr);
+    return result;
+  } catch (error) {
+    console.log('error executeDb', error);
+    throw new Error('error executeDb');
+  } finally {
+    conn?.end();
+  }
+}
+
 async function getAllBooksDb() {
   const sql = 'SELECT * FROM books';
+  return getArrayFromDb(sql);
+}
+async function authorBookCount() {
+  const sql = `
+  SELECT authors.name, authors.surname, COUNT(books.id) AS bookCount FROM authors
+  LEFT JOIN books
+  ON books.author_id = authors.id
+  GROUP BY authors.id
+  `;
   return getArrayFromDb(sql);
 }
 async function allBooksWithAuthors() {
@@ -33,4 +56,19 @@ async function getFirstBook() {
   return getArrayFromDb(sql);
 }
 
-module.exports = { getAllBooksDb, allBooksWithAuthors };
+async function insertBookDb(newBookObj) {
+  const { title, year, author_id } = newBookObj;
+  // executeDb
+  const sql = `
+  INSERT INTO books (title, year, author_id)
+  VALUES (?, ?, ?)
+  `;
+  return executeDb(sql, [title, year, author_id]);
+}
+
+module.exports = {
+  getAllBooksDb,
+  allBooksWithAuthors,
+  insertBookDb,
+  authorBookCount,
+};
--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { getAllBooksDb, allBooksWithAuthors } = require('../model/booksModel');
+const {
+  getAllBooksDb,
+  allBooksWithAuthors,
+  insertBookDb,
+  authorBookCount,
+} = require('../model/booksModel');
 
 const booksRoutes = express.Router();
 // sukuriam booksRoutes routeri
@@ -14,6 +19,15 @@ booksRoutes.get('/books', async (req, res) => {
     res.sendStatus(500);
   }
 });
+booksRoutes.get('/books-count', async (req, res) => {
+  try {
+    const allBooksArr = await authorBookCount();
+    res.json(allBooksArr);
+  } catch (error) {
+    // console.log('stack=== ', error.stack);
+    res.sendStatus(500);
+  }
+});
 // extra booksModel funkcija getAllBooksDB
 
 // GET /books-authors - grazinam visas knygas su autoriu vardais ir pavardem.
@@ -21,6 +35,20 @@ booksRoutes.get('/books-authors', async (req, res) => {
   try {
     const allBooksArr = await allBooksWithAuthors();
     res.json(allBooksArr);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+booksRoutes.post('/books', async (req, res) => {
+  try {
+    const newBookObj = req.body;
+    const createBookResult = await insertBookDb(newBookObj);
+    if (createBookResult.affectedRows === 1) {
+      res.sendStatus(201);
+      return;
+    }
+    res.status(400).json('no book created');
   } catch (error) {
     res.sendStatus(500);
   }
